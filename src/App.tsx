797e6b48d0152a6b90324f52572c55dbb55be580
telafifi <telafifi@gmail.com>
2021-07-13 23:01:40 -0500

    Nav Bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './components/Login';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Switch>
           <Route exact path="/">
             <Login />
